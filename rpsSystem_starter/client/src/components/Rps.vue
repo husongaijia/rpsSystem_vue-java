456537f04667075fbf2b4f71d6df72babea78250
戸松 愛佳 <tomatsu.aika@jp.fujitsu.com>
2019-09-28 15:18:23 +0900

    starterの初回コミット

---
+++ rpsSystem_starter/client/src/components/Rps.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Rps"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ rpsSystem_starter/client/tests/unit/components/Rps.spec.js
@@ -0,0 +1,3 @@
+describe('Rps.vue', () => {
+    
+});
